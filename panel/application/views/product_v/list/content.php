6675e58af5baaf65a0aedbef18f2a74f356a08be
Sena-Kudu <kudusena16@gmail.com>
2020-04-26 14:32:08 +0300

    Toggle button eklendi. Yeni ekle butonu eklendi.Kayıt yoktur alert paneli eklendi.

--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,8 +1,18 @@
 <div class="row">
 	<div class="col-md-12">
-		<h4 class="m-b-lg">Ürün Listesi</h4>
+		<h4 class="m-b-lg">
+		Ürün Listesi
+		<a href="#" class="btn pull-right btn-outline btn-primary btn-xs"> <i class="fa fa-plus">Yeni Ekle</i></a>
+	</h4>
+	</div>
+	<div class="col-md-12">
 		<div class="widget p-lg">
-
+           <div class="alert alert-info text-center ">
+								
+								
+								<p>Burada herhngi bir veri bulunmamaktadır.Eklemek için lütfen <a href="#">tıklayınız.</a></p>
+							</div>
+            
 	
 			<table class="table table-hover table-striped">
 				<thead>
@@ -19,7 +29,8 @@
 					     	<td>lalallala</td>
 					     	<td>mouse</td>
 					     	<td>çok kullanışlı</td>
-					     	<td>1/0</td>
+					     	<td>
+								<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked /></td>
 					     	<td>
 					     		<a href="#" class="btn btn-outline btn-danger">SiL<i class="fa fa-trash"></i><a/>
 					     		<a href="#" class="btn btn-outline btn-info">Düzenle<i class="fa fa-pencil-square-o"></i><a/>
@@ -31,5 +42,6 @@
 				</thead>
 			</table>
 		</div><!-- .widget -->
-	</div><!-- END column -->
+	</div>
+	<!-- END column -->
 </div>
