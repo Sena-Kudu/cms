ffad9b89b4eb23ba4249c938ace95ab3eab452ef
Sena-Kudu <kudusena16@gmail.com>
2020-04-26 16:19:09 +0300

    Ürün listesi veritabanından getirildi ve dinamikleştirildi.Product_model kodlandı.

--- panel/application/controllers/Product.php
+++ panel/application/controllers/Product.php
@@ -8,16 +8,22 @@ class Product extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->viewFolder = "product_v";
-
-
-
+		$this->load->model("product_model");
 
 	}
 	public function index()
 	{
 		$viewData = new stdClass();
-		$viewData->viewFolder = $this->viewFolder;
-		$viewData->subViewFolder = "list";
+		
+
+		/** Tablodan verilerin getirilmesi */
+		$items = $this->product_model->get_all();
+		
+
+		/** View e gönderilecek değişkenlerin set edilmesi */
+		$viewData->viewFolder = $this->viewFolder; /** dosya yolu dinamikleştirimesi*/
+		$viewData->subViewFolder = "list";         /** dosya yolu dinamikleştirimesi*/
+		$viewData->items = $items;
 
 		$this->load->view("{$viewData->viewFolder}/{$viewData->subViewFolder}/index",$viewData);
 	}
--- panel/application/views/product_v/list/content.php
+++ panel/application/views/product_v/list/content.php
@@ -1,47 +1,62 @@
 <div class="row">
 	<div class="col-md-12">
 		<h4 class="m-b-lg">
-		Ürün Listesi
-		<a href="#" class="btn pull-right btn-outline btn-primary btn-xs"> <i class="fa fa-plus">Yeni Ekle</i></a>
-	</h4>
+			Ürün Listesi
+			<a href="#" class="btn pull-right btn-outline btn-primary btn-xs"> <i class="fa fa-plus">Yeni Ekle</i></a>
+		</h4>
 	</div>
 	<div class="col-md-12">
 		<div class="widget p-lg">
-           <div class="alert alert-info text-center ">
-								
-								
-								<p>Burada herhngi bir veri bulunmamaktadır.Eklemek için lütfen <a href="#">tıklayınız.</a></p>
-							</div>
-            
-	
-			<table class="table table-hover table-striped">
-				<thead>
+			<?php if(empty($items)) { ?>
+				<div class="alert alert-info text-center ">
+
+
+					<p>Burada herhngi bir veri bulunmamaktadır.Eklemek için lütfen <a href="#">tıklayınız.</a></p>
+				</div>
+			<?php } else{ ?>
+
+				<table class="table table-hover table-striped">
+					<thead>
 						<th>#id</th>
 						<th>url</th>
 						<th>Başlık</th>
 						<th>Açıklama</th>
 						<th>Durumu</th>
 						<th>İşlem</th>
-					<tbody>
-					
-					     <tr>
-					     	<td>#1</td>
-					     	<td>lalallala</td>
-					     	<td>mouse</td>
-					     	<td>çok kullanışlı</td>
-					     	<td>
-								<input id="switch-2-2" type="checkbox" data-switchery data-color="#10c469" checked /></td>
-					     	<td>
-					     		<a href="#" class="btn btn-outline btn-danger">SiL<i class="fa fa-trash"></i><a/>
-					     		<a href="#" class="btn btn-outline btn-info">Düzenle<i class="fa fa-pencil-square-o"></i><a/>
+						<tbody>
+							<?php foreach ($items as $items) {
+							# code...
+								?>	
+								<tr>
+									<td><?php echo $items->id; ?></td>
+									<td><?php echo $items->url; ?></td>
+									<td><?php echo $items->title; ?></td>
+									<td><?php echo $items->description; ?></td>
+									<td>
+										<input 
+										 
+										type="checkbox" 
+										data-switchery data-color="#10c469" 
+										<?php if($items->isActive == "1"){ 
 
-					     	</td>
-					     </tr>
-						
-					</tbody>
-				</thead>
-			</table>
-		</div><!-- .widget -->
-	</div>
-	<!-- END column -->
-</div>
+											echo "checked"; /*Buradaki "checked" komutu aktifliği sağlar.Veritabanındaki 0/1 değerlerine göre buradaki aktiflik belirlendi. */
+										} 
+										else { echo "";}
+										?> />
+
+									</td>
+									<td>
+										<a href="#" class="btn btn-outline btn-danger">SiL<i class="fa fa-trash"></i><a/>
+											<a href="#" class="btn btn-outline btn-info">Düzenle<i class="fa fa-pencil-square-o"></i><a/>
+
+											</td>
+										</tr>
+									<?php } ?>
+								</tbody>
+							</thead>
+						</table>
+					<?php } ?>
+				</div><!-- .widget -->
+			</div>
+			<!-- END column -->
+		</div>
