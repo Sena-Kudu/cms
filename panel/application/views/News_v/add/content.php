f1f6ff7d43f15388b7d1b9d5770a19687578137d
Sena-Kudu <kudusena16@gmail.com>
2020-06-02 02:16:28 +0300

    Haber ekle sayfasının tasarımı yapıldı.

--- panel/application/views/News_v/add/content.php
+++ panel/application/views/News_v/add/content.php
@@ -10,14 +10,14 @@
 
 			<div class="widget-body">
 
-				<form action = "<?php echo base_url("product/save"); ?>" method = "post">
+				<form action = "<?php echo base_url("news/save"); ?>" method = "post">
 					<div class="form-group">
 						<label>Başlık</label>
 						<input class="form-control" name="title" placeholder="Başlık">
 						<?php if(isset($form_error)) { ?>
-						<small class= "input-form-error"> <?php echo form_error("title"); ?></small>
+							<small class= "input-form-error"> <?php echo form_error("title"); ?></small>
 
-					<?php } ?>
+						<?php } ?>
 					</div>
 
 					<div class="form-group">
@@ -26,9 +26,32 @@
 						
 					</div>
 
+					<div class="form-group">
+						<label for="control-demo-6" class="">Haberin Türü</label>
+						<div id="control-demo-6" class="" name = "news_type">
+							<select class="form-control">
+								<option value = "image">Resim</option>
+								<option value = "video">Video</option>
+								
+							</select>
+						</div>
+					</div><!-- .form-group -->
+					<div class="form-group">
+						<label >Görsel Seçiniz</label>
+						<input type="file" name = "image_url" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>Video URL</label>
+						<input class="form-control" name="video_url" placeholder="Video bağlantısını buraya yapıştırınız.">
+						<?php if(isset($form_error)) { ?>
+							<small class= "input-form-error"> <?php echo form_error("video_url"); ?></small>
+
+						<?php } ?>
+					</div>
+
 
 					<button type="submit" class="btn btn-primary btn-md btn-outline">Kaydet</button>
-					<a href = "<?php echo base_url("product"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
+					<a href = "<?php echo base_url("news"); ?>" class="btn btn-md btn-danger btn-outline">İptal</a>
 				</form>
 			</div><!-- .widget-body -->
 		</div><!-- .widget -->
