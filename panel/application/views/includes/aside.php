97b86d54c8e6f21485db50ddeb89eb963bdcf9bd
Sena-Kudu <kudusena16@gmail.com>
2020-11-29 15:01:04 +0300

    Markalar modülü tamamlandı.

--- panel/application/views/includes/aside.php
+++ panel/application/views/includes/aside.php
@@ -115,7 +115,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="javascript:void(0)">
+                    <a href="<?php echo base_url("Brands"); ?>">
                         <i class="menu-icon zmdi zmdi-puzzle-piece zmdi-hc-lg"></i>
                         <span class="menu-text">Markalar</span>
                     </a>
